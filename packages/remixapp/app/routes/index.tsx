011e6f7ff38d1d1e6fe00dc1d1ba6c9bcb34736c
Orest Hazda <oresthazda@gmail.com>
2023-01-08 20:49:58 -0500

    chore: test cache

--- packages/remixapp/app/routes/index.tsx
+++ packages/remixapp/app/routes/index.tsx
@@ -1,14 +1,12 @@
-import {Header} from 'header';
-import {Footer} from 'footer';
+import { Header } from 'header';
+import { Footer } from 'footer';
 
 export default function Index() {
-    return (
-        <>
-            <Header/>
-            <div>
-                Content!
-            </div>
-            <Footer/>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <div>New Content!</div>
+      <Footer />
+    </>
+  );
 }
